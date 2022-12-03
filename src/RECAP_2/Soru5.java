844210aed8ccde8541763773413faf7aca8ae0f0
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-03 21:28:59 +0100

    java3

--- src/RECAP_2/Soru5.java
+++ src/RECAP_2/Soru5.java
@@ -1,4 +1,13 @@
 package RECAP_2;
 
-public class Soru5 {
+import java.util.Scanner;
+
+public class Soru5 { //kullanicidan ismini ve soyismini girmesini isteyin,sonrasinda tam ismini
+    //buyuk harfle yazdirin
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("lutfen isminizi ve soy isminizi giriniz");
+        String isimSoy= scan.nextLine().toUpperCase();
+        System.out.println("isim soyisim :" +isimSoy);
+    }
 }
