14d265399c83405649382b81c6a5ce183d63b642
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-05 21:57:05 +0100

    javaz

---
+++ src/ZZZ/C01.java
@@ -0,0 +1,13 @@
+package ZZZ;
+
+public class C01 {public static void main(String[] args) {
+    int not1=60;
+    int not2=70;
+
+    not2=90;
+
+    int ort=(not1+not2)/2;
+
+    System.out.println(ort);
+}
+}
