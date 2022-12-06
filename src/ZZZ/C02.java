afac76bea8e1e87aecdd384a2b20ba5149f9d498
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-06 21:10:39 +0100

    java2

---
+++ src/ZZZ/C02.java
@@ -0,0 +1,17 @@
+package ZZZ;
+
+public class C02 {public static void main(String[] args) {
+
+    int sayi=10;
+    boolean guzelMi=true;
+    char sayim='3';
+
+    System.out.println(sayi);
+
+    System.out.println("sayi : " + sayi);
+
+    String isim="ilknur";
+    String soyIsim="Yilmaz";
+
+    System.out.println("isim : " + isim ); System.out.println("soyIsim :" + soyIsim);
+}}
