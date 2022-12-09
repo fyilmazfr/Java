cd88af4ad69a154651208619b48926fb4031015f
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-09 13:15:24 +0100

    java

---
+++ src/day16_ForLoop/C03_forLOOP.java
@@ -0,0 +1,11 @@
+package day16_ForLoop;
+
+public class C03_forLOOP {// SORU: 10 ILE 30 arasindaki tam sayilari aralarinda virgul koyarak yazdirin
+
+    public static void main(String[] args) {
+        for (int i = 10; i <=30 ; i++) {
+            System.out.print(i+ ",");
+
+        }
+    }
+}
