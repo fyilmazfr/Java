22a5306f9e26e048646fc32c8c5a291712d07147
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-09 11:18:55 +0100

    java

---
+++ src/day15_Overloading_forLoop/C07_ForLoop.java
@@ -0,0 +1,8 @@
+package day15_Overloading_forLoop;
+
+public class C07_ForLoop {// C06 da olusturdugumuz tersten yazdir methodunu cagiralim
+
+    public static void main(String[] args) {
+        C06_StringTerseCevirme.terstenYazdir("hersey cok guzel olacak");
+    }
+}
