f326af69d1d3790d29ca682aa982203c818aa7f0
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-11 16:20:01 +0100

    java

---
+++ src/EKSTRA_SORULAR/C12.java
@@ -0,0 +1,22 @@
+package EKSTRA_SORULAR;
+
+import java.util.Scanner;
+
+public class C12 {////Bu örneğimizde Herhangi Bir Sayının Katlarını Bulacağız.
+
+    public static void main(String[] args) {
+
+         int input=120;
+        for (int i = 0; i <input ; i++) {
+            if(i%2==0){
+                System.out.print(i+",");
+            }
+
+        }
+
+
+
+
+
+
+}}
