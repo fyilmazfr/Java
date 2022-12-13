9c964d07870981a9815e6c1231dedb87e2ffd96d
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-13 20:13:51 +0100

    J

---
+++ src/EKSTRA_SORULAR/C16_ForLoop.java
@@ -0,0 +1,12 @@
+package EKSTRA_SORULAR;
+
+public class C16_ForLoop {//ekrana 10 kez java guzeldir yazdirin.
+
+    public static void main(String[] args) {
+        int sayac=0;
+        for (int i = 0; i <=10 ; i++) {
+            System.out.print("java guzeldir"+",");
+
+        }
+    }
+}
