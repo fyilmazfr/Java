1f368c38c1c7aa5bb2d036689856744c124e7da5
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-13 20:18:04 +0100

    J

---
+++ src/EKSTRA_SORULAR/C17_ForLoop.java
@@ -0,0 +1,11 @@
+package EKSTRA_SORULAR;
+
+public class C17_ForLoop {//10 ILE 20 arasinda ki sayilari arasinda virgul olacak sekilde ayni satirda yazdirin.
+
+    public static void main(String[] args) {
+        for (int i = 10; i <=20 ; i++) {
+            System.out.print(i+",");
+
+        }
+    }
+}
