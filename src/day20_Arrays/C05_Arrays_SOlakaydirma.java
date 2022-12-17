4c3ecb343a06dacfccd0f4972116055e8e959a55
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-17 21:40:37 +0100

    J

---
+++ src/day20_Arrays/C05_Arrays_SOlakaydirma.java
@@ -0,0 +1,31 @@
+package day20_Arrays;
+
+import java.util.Arrays;
+
+public class C05_Arrays_SOlakaydirma {// VERILEN 3 ELEMANLI BIR ARRAY'in
+    //tum elemanlarini bir soldaki konuma tasiyacak bir program yazin.
+    //ornek;array[1,2,3] ise output [2,3,1] olacak
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};// burayi degistirebiliriz
+        int temps=arr[0];// son degri buraya atamak icin
+        for (int i = 0; i < arr.length-1 ; i++) {
+            arr[i]=arr[i+1];
+        }
+        arr[arr.length-1]=temps;// her defasinda 0'inci indeksi yani temp'i arr'ye son indekse  ata
+        System.out.println(Arrays.toString(arr));
+
+        //yukarida degistirdigimiz arrayi ilk duruma getirmek icin saga kaydiralim
+           temps=arr[arr.length-1];
+        for (int i = arr.length-1; i >0 ; i--) {
+            arr[i] = arr[i - 1];
+        }
+            arr[0]=temps;
+            System.out.println(Arrays.toString(arr));
+
+        }
+
+        }
+
+
+
+
