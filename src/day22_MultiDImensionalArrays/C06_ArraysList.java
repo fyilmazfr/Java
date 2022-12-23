63302cff3ea7274b4fa7af82818b0e007d40eb6c
Fatih Yilmaz <fylmz7106@hotmail.com>
2022-12-23 15:39:14 +0100

    J

---
+++ src/day22_MultiDImensionalArrays/C06_ArraysList.java
@@ -0,0 +1,22 @@
+package day22_MultiDImensionalArrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C06_ArraysList {/* array'i array yapan sembol[] idi,
+arrayList de ise <> diamonde (elmas ) kullaniriz.
+*/
+
+    public static void main(String[] args) {
+        List<String> isimler=new ArrayList<>();// ArrayList yazmak zorundayiz
+        System.out.println(isimler);//direk isimler yazmamiz yetiyor java bunu yazdirir
+        //bir list'e eleman eklemek istersek
+        isimler.add("fatih"); // fatih'i eklemis olduk bize tru yada false donecektir
+
+        System.out.println(isimler.add("melih"));//true doner
+        System.out.println(isimler);//[fatih, melih] listeye ekledi
+        //list'in tek kotu tarafi array'in alt yapisini kullandigi icin elemanlari birer birer eklemek gerekir
+
+
+    }
+}
