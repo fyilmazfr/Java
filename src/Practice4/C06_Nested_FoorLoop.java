37c4973f2a643433224932de1651468c47efe79d
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-01-01 15:12:41 +0100

    J

---
+++ src/Practice4/C06_Nested_FoorLoop.java
@@ -0,0 +1,26 @@
+package Practice4;
+
+public class C06_Nested_FoorLoop {////yukaridaki carpim tablosunu 10lara kadar ekrana yazdiriniz
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <=10 ; i++) {//auter for
+            for (int j = 1; j <=10 ; j++) {//inner for,ic taraf, her defasinda artacak
+                System.out.println(i+"*"+j+"="+(i*j));
+
+            }
+            System.out.println();//bosluk attik
+            System.out.println();//bosluk attik
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
