dda79b860930a34af6761c2e19ad5ed599a5e93e
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-01-07 09:04:22 +0100

    J

---
+++ src/Practice6/Q01_WhileLoop_rakamlarToplam.java
@@ -0,0 +1,24 @@
+package Practice6;
+
+import java.util.Scanner;
+
+public class Q01_WhileLoop_rakamlarToplam {//girilen sayinin basamaklarindaki rakamlarin toplamini bulunuz
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("lutfen bir sayi giniz");
+        int sayi= scan.nextInt();
+        int birlerbasamagi;
+        int rakamlarToplami=0;
+
+        while (sayi>0){
+            birlerbasamagi=sayi%10;
+            rakamlarToplami+=birlerbasamagi;
+            sayi=sayi/10;
+
+
+        }
+        System.out.println("girilen sayinin rakamlari toplami :"+rakamlarToplami);
+    }
+
+}
