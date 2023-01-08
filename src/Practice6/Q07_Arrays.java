1dcd92399c1d195d488976996810e0200444c4d0
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-01-08 08:30:08 +0100

    J

---
+++ src/Practice6/Q07_Arrays.java
@@ -0,0 +1,26 @@
+package Practice6;
+
+import java.util.Arrays;
+
+public class Q07_Arrays {/*verilen arrayde toplamlari istenen degere esit olan sayi ciftlerini yazdiran
+    bir method yaziniz , asagida istenen toplam deger 9 olsun.*/
+
+    public static void main(String[] args) {
+        int [] arr={5,7,-6,4,2,15,3,8,1};
+        int istenenToplam=9;
+
+
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j =0; j < arr.length ; j++) {
+
+                if (arr[i]+arr[j]==istenenToplam){
+
+                    System.out.println(arr[i]+"ve"+arr[j]+"toplam:"+istenenToplam);
+                }
+
+            }
+
+        }
+
+    }
+}
