3e3e7705c684ffa2abe53e1d12da4a4fd8c90b20
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-01-20 20:37:28 +0100

    J

--- src/OrnekSorularARRAYS_slymn/soru3.java
+++ src/OrnekSorularARRAYS_slymn/soru3.java
@@ -1,6 +1,7 @@
 package OrnekSorularARRAYS_slymn;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class soru3 {
     public static void main(String[] args) {
@@ -18,5 +19,23 @@ public class soru3 {
 
         }
         System.out.println(Arrays.toString(temps));
+
+        //EXAMPLE 2: bir array in icinde bir elemanin olup olmadigini kontrol eden ,ve kac kere tekrarlandigini
+        // gosteren kodu yaziniz.
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen bir tam sayi belirtiniz ve yanit bekleyiniz");
+        int sayi = scan.nextInt();
+        int sayac = 0;
+
+
+        int[] xrr = {0, 2, 3, 0, 12, 0, 4, 8, 8, 8, 0, 0};
+        for (int i = 0; i < xrr.length; i++) {
+            if (xrr[i] == sayi) {
+                sayac++;
+            }
+
+        }
+        System.out.println("girilen sayi " + sayac + " " + " kez kullanilmistir");
     }
 }
