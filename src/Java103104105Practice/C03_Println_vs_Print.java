b95310f6b8a2bb3f0a6be3f10410b60cb3f5fa6e
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-03-07 20:30:15 +0100

    J

---
+++ src/Java103104105Practice/C03_Println_vs_Print.java
@@ -0,0 +1,22 @@
+package Java103104105Practice;
+
+import java.util.Arrays;
+
+public class C03_Println_vs_Print {
+    /*konsolda 12345 'i  ALT ALTA VE YANYANA YAZDIRINIZ
+
+     */
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.print("print*******; " + "12345");
+
+
+
+
+}
+
+}
