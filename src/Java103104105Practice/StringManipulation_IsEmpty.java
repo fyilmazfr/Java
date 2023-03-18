f441bb6821f699718a381ff5a48f6c457e45d567
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-03-18 11:24:48 +0100

    J

---
+++ src/Java103104105Practice/StringManipulation_IsEmpty.java
@@ -0,0 +1,16 @@
+package Java103104105Practice;
+
+public class StringManipulation_IsEmpty {
+    public static void main(String[] args) {
+        String str1="hi";
+        System.out.println(str1.isEmpty());//false
+
+        String str2="";
+        System.out.println(str2.isEmpty());//true
+        String str3=" ";
+        System.out.println(str3.isEmpty());//false
+
+
+    }
+
+}
