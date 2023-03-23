aa335ce04d04efb7315ddf95323abdcfe64a986a
Fatih Yilmaz <fylmz7106@hotmail.com>
2023-03-23 10:33:21 +0100

    J

---
+++ src/Java103104105Practice/FoorLoop.java
@@ -0,0 +1,28 @@
+package Java103104105Practice;
+
+import java.util.Scanner;
+
+public class FoorLoop {
+    /*
+    kullanicidan bir cumle ve bir harf alin
+    cumlede verilen harfin kac kere kullanildigini bulup yazdirin
+
+     */
+    public static void main(String[] args) {
+        String input="Java ogrenmek cok guzel";
+       char HARF='e';
+       int sayac=0;
+
+        for (int i = 0; i <input.length() ; i++) {
+
+            if (input.charAt(i)==HARF){
+                sayac++;
+            }
+        }
+        System.out.println("girdiginiz cumlede "+HARF+ " harfi "+sayac+ "tanedir");
+
+        }
+
+
+    }
+
